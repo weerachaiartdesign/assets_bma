5e9238574030abcf23b5c9ca697c50a241292a85
weerachaiartdesign <weerachaiartdesign@gmail.com>
2026-03-18 16:46:05 +0700

    Add files via upload

---
+++ Assets_BMA/js/api-config.js
@@ -0,0 +1,2 @@
+// ใส่ URL ของ Google Apps Script ที่คุณทำการ Deploy ล่าสุด (Deploy -> New Deployment)
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbzuHyJ7qZNecpsAxPJNBWh_S0vYXtETTo5rrzLjUAULGV3imbw_qp28Es1_fPrtfEQq/exec";

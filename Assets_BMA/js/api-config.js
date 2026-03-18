72a2a6b798418d02e4dcb736da30edb0d1ac2055
weerachaiartdesign <weerachaiartdesign@gmail.com>
2026-03-18 17:27:47 +0700

    002Update api-config.js

--- Assets_BMA/js/api-config.js
+++ Assets_BMA/js/api-config.js
@@ -1,2 +1,2 @@
 // ใส่ URL ของ Google Apps Script ที่คุณทำการ Deploy ล่าสุด (Deploy -> New Deployment)
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbw-KBWLdboZuWXO9YQMfJertn0UZ4Y0HRRqWvT6Xn2doHbyEOPo8UFbyWii58r_TN44/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycby3U8hHPjOiUsiT014xlDps_L__f0Xqi__eegrL5zMMFDBeJl38Q6Jlm20DaUFuBX25/exec";
